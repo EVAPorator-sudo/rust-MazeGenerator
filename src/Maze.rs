f37624d81f3106a59c893798df828e9be183ca7b
evaporator <osxar.jeavons@gmail.com>
2026-03-27 19:44:30 +0000

    fixed constructors, switched all dimensions to usize for no casting, added tests for each class

--- src/Maze.rs
+++ src/Maze.rs
@@ -1,22 +1,22 @@
 pub struct Grid {
-    width: i32,
-    height: i32,
-    row_list: Vec<Row>,
+    pub width: usize,
+    pub height: usize,
+    pub row_list: Vec<Row>,
 }
 
 pub struct Row {
-    width: i32,
-    y_positon: i32,
-    cell_list: Vec<Cell>,
+    pub width: usize,
+    pub y_positon: usize,
+    pub cell_list: Vec<Cell>,
 }
 
 pub struct Cell {
-    position: [i32; 2],
-    walls: [i8; 3],
+    pub position: [usize; 2],
+    pub walls: [i8; 3],
 }
 
 impl Cell {
-    pub fn new(XPos: i32, YPos: i32) -> Self {
+    pub fn new(XPos: usize, YPos: usize) -> Self {
         Cell {
             position: [XPos, YPos],
             walls: [0, 0, 0],
@@ -25,10 +25,10 @@ impl Cell {
 }
 
 impl Row {
-    pub fn new(Width: i32, y_position: i32) -> Self {
+    pub fn new(Width: usize, y_position: usize) -> Self {
         let mut cell_List: Vec<Cell> = Vec::new();
 
-        for index in 0..(Width - 1) {
+        for index in 0..Width {
             cell_List.push(Cell::new(index, y_position));
         }
 
@@ -41,10 +41,10 @@ impl Row {
 }
 
 impl Grid {
-    pub fn new(Width: i32, Height: i32) -> Self {
+    pub fn new(Width: usize, Height: usize) -> Self {
         let mut row_List: Vec<Row> = Vec::new();
 
-        for index in 0..(Width - 1) {
+        for index in 0..Height {
             row_List.push(Row::new(Width, index));
         }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,74 @@
 pub mod Maze;
+
+#[cfg(test)]
+mod MazeTests {
+
+    use super::Maze::{Cell, Grid, Row};
+
+    const cases: [[usize; 2]; 6] = [[0, 0], [10, 10], [50, 10], [10, 50], [50, 0], [10, 50]];
+
+    #[test]
+    fn cell_constructor() {
+        for case in cases {
+            Cell::new(case[0], case[1]);
+        }
+    }
+
+    #[test]
+    fn row_constructor() {
+        for case in cases {
+            Row::new(case[0], case[1]);
+        }
+    }
+
+    #[test]
+    fn grid_constructor() {
+        for case in cases {
+            Grid::new(case[0], case[1]);
+        }
+    }
+
+    #[test]
+    fn cell_assignment() {
+        for case in cases {
+            let cell = Cell::new(case[0], case[1]);
+
+            assert_eq!(cell.position, case);
+            assert_eq!(cell.walls, [0; 3]);
+        }
+    }
+
+    #[test]
+    fn row_assignment() {
+        for case in cases {
+            let row = Row::new(case[0], case[1]);
+
+            assert_eq!(row.width, case[0]);
+            assert_eq!(row.y_positon, case[1]);
+            assert_eq!(row.cell_list.len(), case[0]);
+
+            if (case[0] != 0 && case[1] != 0) {
+                let test_cell = row.cell_list.get(0).unwrap();
+                let expected_coordinates = [0, case[1]];
+                assert_eq!(test_cell.position, expected_coordinates);
+            }
+        }
+    }
+
+    #[test]
+    fn grid_assignment() {
+        for case in cases {
+            let grid = Grid::new(case[0], case[1]);
+
+            assert_eq!(grid.width, case[0]);
+            assert_eq!(grid.height, case[1]);
+            assert_eq!(grid.row_list.len(), case[1]);
+
+            if case[0] != 0 && case[1] != 0 {
+                let test_row = grid.row_list.get(0).unwrap();
+                assert_eq!(test_row.width, case[0]);
+                assert_eq!(test_row.y_positon, 0);
+            }
+        }
+    }
+}
