5863269685ae6d5520451dd79ca9318be794e7f7
evaporator <osxar.jeavons@gmail.com>
2026-03-29 22:25:18 +0100

    added user parameter input

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 use MazeGenerator::{Draw::grid_draw, Generator::*, Maze::Grid::Grid};
-use std::fs;
+use core::panic;
+use std::{env, fs};
 
 fn main() {
-    let grid = Ellers(Grid::new(1000, 1000));
-    let svg = grid_draw(&grid);
-    fs::write("maze_image_e.svg", svg).expect("failed to write SVG");
+    let args: Vec<String> = env::args().collect();
 
-    let grid = Growing_Tree(Grid::new(1000, 1000), 0.8);
-    let svg = grid_draw(&grid);
-    fs::write("maze_image_gt.svg", svg).expect("failed to write SVG");
+    if args.len() < 5 {
+        panic!("invalid args")
+    }
+
+    let mode = &args[1];
+    let width: usize = args[2].parse().expect("width must be a positive integer");
+    let height: usize = args[3].parse().expect("height must be a positive integer");
+
+    let grid: Grid;
+    let path;
+
+    let grid;
+    match mode.as_str() {
+        "e" => {
+            grid = Ellers(Grid::new(width, height));
+            path = &args[4];
+        }
+        "g" => {
+            grid = Growing_Tree(
+                Grid::new(width, height),
+                args[4]
+                    .parse()
+                    .expect("weight must be a decimal between 0 and 1"),
+            );
+            path = &args[5];
+        }
+        _ => panic!("invalid algorithm"),
+    }
+
+    fs::write(path, grid_draw(&grid)).expect("error generating maze");
 }
