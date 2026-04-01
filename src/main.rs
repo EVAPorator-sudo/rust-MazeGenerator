e50b57aac760a332a56578e9c46e8fe27c2d660b
evaporator <osxar.jeavons@gmail.com>
2026-04-01 15:36:58 +0100

    added flag env variables and cleaned up main function

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,28 @@ fn main() {
         panic!("invalid args")
     }
 
-    let mode = &args[1];
     let width: usize = args[2].parse().expect("width must be a positive integer");
     let height: usize = args[3].parse().expect("height must be a positive integer");
 
-    let grid: Grid;
-    let path;
-
-    let grid;
-    match mode.as_str() {
-        "e" => {
-            grid = Ellers(Grid::new(width, height));
-            path = &args[4];
+    let grid = match args[1].as_str() {
+        "--e" => {
+            if args.len() < 5 {
+                panic!("invalid number of parameters")
+            }
+            Ellers(Grid::new(width, height))
         }
-        "g" => {
-            grid = Growing_Tree(
+        "--g" => {
+            if args.len() < 6 {
+                panic!("invalid number of parameters")
+            }
+            Growing_Tree(
                 Grid::new(width, height),
                 args[4]
                     .parse()
                     .expect("weight must be a decimal between 0 and 1"),
-            );
-            path = &args[5];
+            )
         }
         _ => panic!("invalid algorithm"),
-    }
-
-    fs::write(path, grid_draw(&grid)).expect("error generating maze");
+    };
+    fs::write(args.last().unwrap(), grid_draw(&grid)).expect("error generating maze");
 }
