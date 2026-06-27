8c60073a7bb5145ab7f7280a1cf06d19a6e070d1
EVAPorator <osxar.jeavons@gmail.com>
2026-06-27 16:23:46 +0100

    added valid_directions method to grid class

--- src/Maze/Grid.rs
+++ src/Maze/Grid.rs
@@ -271,4 +271,28 @@ impl Grid {
             }
         }
     }
+
+    pub fn valid_directions(&self, coords: [usize; 2]) -> Vec<directions> {
+        let mut dirs = vec![];
+
+        if coords[0] == 0 {
+            dirs.push(right);
+        } else if coords[0] == self.width -1 {
+            dirs.push(left);
+        } else {
+            dirs.push(left);
+            dirs.push(right);
+        }
+
+        if coords[1] == 0 {
+            dirs.push(down);
+        } else if coords[1] == self.height -1 {
+            dirs.push(up);
+        } else {
+            dirs.push(down);
+            dirs.push(up);
+        }
+
+        dirs
+    }
 }
